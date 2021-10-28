3ed5699d8f265aa43240c0b6028eeb0e91e404d5
Noelia Martínez <noeowo384@gmail.com>
2021-10-28 18:01:54 -0400

    transición de escenas

--- Assets/Scripts/ScenaManager.cs
+++ Assets/Scripts/ScenaManager.cs
@@ -5,16 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class ScenaManager : MonoBehaviour
 {
+	public Animator transition;
+	public float transitionTime = 1f;
     // Start is called before the first frame update
     //void Start(){}
 
     // Update is called once per frame
     // void Update(){}
-    public void Manager (string data){
-        SceneManager.LoadScene(data);
+    public void Manager (string SceneData){
+        StartCoroutine("LoadLevel");
+		SceneManager.LoadScene(SceneData);
     }
 
     public void Exit (){
         Application.Quit();
     }
+	
+	IEnumerator LoadLevel(){
+		transition.SetTrigger("Start");
+		yield return new WaitForSeconds(transitionTime);
+	}
 }
