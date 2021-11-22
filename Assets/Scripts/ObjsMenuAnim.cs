3a6b28d8913ea74045f573211b6c816e0710938d
Krows-MJ <lukakrows@gmail.com>
2021-11-22 12:44:46 -0400

    puliendo el menu

--- Assets/Scripts/ObjsMenuAnim.cs
+++ Assets/Scripts/ObjsMenuAnim.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ObjsMenuAnim : MonoBehaviour
 {
-    //inicio:-100 /-\ fin:680
+    //inicio:-110 /-\ fin:680
     [Range(50f,200f)]public float vel;
     private Animator anim;
     // Start is called before the first frame update
@@ -18,7 +18,7 @@ public class ObjsMenuAnim : MonoBehaviour
     {
         transform.position= new Vector3(transform.position.x+vel*Time.deltaTime, 
                                         transform.position.y, transform.position.z);
-        if(transform.position.x>=680) transform.position= new Vector3(-100f,transform.position.y,transform.position.z);   
+        if(transform.position.x>=2030) transform.position= new Vector3(-110f,transform.position.y,transform.position.z);   
     }
     public void PlayGame(){
         vel=0f;
